700561843a5a0193af81c0d75597d7a4a392e7bc
Annette Jensen <annetteduedal@gmail.com>
2025-09-29 20:39:18 +0200

    Opdateret bindings i renterinfoview

---
+++ Reolmarked/ViewModel/Helpers/RentalContractDisplayModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reolmarked.Model;
+
+namespace Reolmarked.ViewModel.Helpers
+{
+    public class RentalContractDisplayModel : ViewModelBase
+    {
+        private readonly RentalContract _contract;
+
+        public RentalContractDisplayModel(RentalContract contract)
+        {
+            _contract = contract ?? throw new ArgumentNullException(nameof(contract));
+        }
+
+        public string ShelfName => $"Reol {_contract.RackId}";
+
+        public string Period =>
+            $"{_contract.StartDate:dd. MMM yyyy}" +
+            (_contract.EndDate.HasValue ? $" – {_contract.EndDate:dd. MMM yyyy}" : " – aktiv");
+    }
+}
